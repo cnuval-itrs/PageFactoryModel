8dec6da8c3d8f8c6e638af82663e632da0425744
cnuval-itrs <cnuval@itrsgroup.com>
2021-07-24 01:20:37 +0800

    Added pipeline code

--- Toptal/src/test/java/com/toptal/tests/GoToHomePageTest.java
+++ Toptal/src/test/java/com/toptal/tests/GoToHomePageTest.java
@@ -1,11 +1,13 @@
 package com.toptal.tests;
 
 import com.toptal.webpages.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -15,7 +17,8 @@ public class GoToHomePageTest {
 
     @Before
     public void setup(){
-        driver = new FirefoxDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
